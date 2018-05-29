cd9755542b14cebb7ca844e64e1b8b3786f819f5
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-05-29 18:52:15 +0300

    Implement geometry function to find intersection point

--- geometry.py
+++ geometry.py
@@ -1,5 +1,22 @@
-# def intersection(point1, point2, flat):
+# Find point of intersection
+#
+def intersection(point1, point2, flat):
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
 
+    equation, (A, B, C, D) = flat
+
+    m = x2 - x1
+    n = y2 - y1
+    p = z2 - z1
+
+    t = -(A * x1 + B * y1 + C * z1 + D) / (A * m + B * n + C * p)
+
+    x = x1 + m * t
+    y = y1 + n * t
+    z = z1 + p * t
+
+    return(x, y, z)
 
 # Builds flat equation for given points
 #
@@ -11,12 +28,13 @@ def flat(points):
     A = (y2 - y1) * (z3 - z1) - (y3 - y1) * (z2 - z1)
     B = (x3 - x1) * (z2 - z1) - (x2 - x1) * (z3 - z1)
     C = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
+    D = -(A * x1 + B * y1 + C * z1)
 
     # Equation to determine whether point belongs to flat
     #
     def equation(point):
         x, y, z = point
-        expression = (x - x1) * A + (y - y1) * B + (z - z1) * C
+        expression = x * A + y * B + z * C + D
         return expression == 0
 
-    return equation
+    return equation, (A, B, C, D)
--- main.py
+++ main.py
@@ -9,3 +9,6 @@ size = (24, 24)
 distance = 12
 
 imagePlane = tr.buildImagePlane(size, cameraPos, distance)
+
+equations = [geom.flat(flat) for flat in flats]
+geom.intersection(cameraPos, (0, 12, 0), equations[0]) # (0, 14, 0) for cube.obj
